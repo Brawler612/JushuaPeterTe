c3377183e2caef2c180561167037fc99a4d7daaf
Brawler612 <brawler612@gmail.com>
2024-04-24 23:40:55 +0800

    Updated

--- src/main/java/com/example/jushuapeterte/Main.java
+++ src/main/java/com/example/jushuapeterte/Main.java
@@ -1,16 +1,35 @@
 package com.example.jushuapeterte;
 
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class Main {
-    public static void main() {
-        System.out.println("Enter windspeed: ");
-        Scanner sc = new Scanner(System.in);
-        int ws = sc.nextInt();
-        if (ws < 62) {
-            System.out.println("Tropical Depression");
-        } else if (ws <= 88) {
-            System.out.println("Tropical Storm");
-        }
+public class Main extends Application {
+    private double xOffset = 0.0;
+    private double yOffset = 0.0;
+
+    public Main() {
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = (Parent)FXMLLoader.load(this.getClass().getResource("Main.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        root.setOnMousePressed((event) -> {
+            this.xOffset = event.getSceneX();
+            this.yOffset = event.getSceneY();
+        });
+        root.setOnMouseDragged((event) -> {
+            primaryStage.setX(event.getScreenX() - this.xOffset);
+            primaryStage.setY(event.getScreenY() - this.yOffset);
+        });
+        primaryStage.setScene(new Scene(root, 780.0, 550.0));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
