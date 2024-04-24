ca6c200bbcc132f61cdc62705fdedfcd7e1f843e
Brawler612 <brawler612@gmail.com>
2024-04-25 00:06:34 +0800

    Updated

--- src/main/java/com/example/jushuapeterte/Controller.java
+++ src/main/java/com/example/jushuapeterte/Controller.java
@@ -1,9 +1,43 @@
 package com.example.jushuapeterte;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
     public Controller() {
+        @FXML
+        private void handleHomeButton(ActionEvent event) throws IOException {
+            loadFXML("Home.fxml");
+        }
+
+        @FXML
+        private void handlePage01Button(ActionEvent event) throws IOException {
+            loadFXML("Page01.fxml");
+        }
+
+        @FXML
+        private void handlePage02Button(ActionEvent event) throws IOException {
+            loadFXML("Page02.fxml");
+        }
+
+        @FXML
+        private void handlePage03Button(ActionEvent event) throws IOException {
+            loadFXML("Page03.fxml");
+        }
+
+        private void loadFXML(String fxmlFile) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
     }
 
     public void up(ActionEvent e) {
--- src/main/java/com/example/jushuapeterte/Main.java
+++ src/main/java/com/example/jushuapeterte/Main.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.IOException;
+
 public class Main extends Application {
     private double xOffset = 0.0;
     private double yOffset = 0.0;
@@ -14,16 +16,16 @@ public class Main extends Application {
     public Main() {
     }
 
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = (Parent)FXMLLoader.load(this.getClass().getResource("Main.fxml"));
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
-        root.setOnMousePressed((event) -> {
-            this.xOffset = event.getSceneX();
-            this.yOffset = event.getSceneY();
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
         });
-        root.setOnMouseDragged((event) -> {
-            primaryStage.setX(event.getScreenX() - this.xOffset);
-            primaryStage.setY(event.getScreenY() - this.yOffset);
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
         });
         primaryStage.setScene(new Scene(root, 780.0, 550.0));
         primaryStage.show();
